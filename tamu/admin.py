b491403fb9bf59c369d157a967c4cbef80ce50d6
Prana dhikasara <pranadhika22@gmail.com>
2022-03-04 10:50:50 +0700

    update admin

--- tamu/admin.py
+++ tamu/admin.py
@@ -4,6 +4,6 @@ from import_export.admin import ExportActionMixin
 from tamu.models import Tamu
 # Register your models here.
 class TamuAdmin(ExportActionMixin,admin.ModelAdmin):
-    list_tamu = ('id', 'date', 'name', 'origin of company', 'assurance', 'necessary', 'appointment with', 'arrival time', 'departure time', 'photo')
+    list_display = ('id', 'date', 'name', 'origin_of_company', 'assurance', 'necessary', 'appointment_with', 'arrival_time', 'departure_time', 'photo')
 
 admin.site.register(Tamu, TamuAdmin)
