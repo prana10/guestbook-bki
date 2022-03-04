6a3e63b551642bad85a8ac8ed45df4a00af3205a
Prana dhikasara <pranadhika22@gmail.com>
2022-03-04 11:03:35 +0700

    update search

--- tamu/admin.py
+++ tamu/admin.py
@@ -5,5 +5,6 @@ from tamu.models import Tamu
 # Register your models here.
 class TamuAdmin(ExportActionMixin,admin.ModelAdmin):
     list_display = ('id', 'date', 'name', 'origin_of_company', 'assurance', 'necessary', 'appointment_with', 'arrival_time', 'departure_time', 'photo')
+    search_fields = ['id', 'date', 'name', 'origin_of_company', 'assurance', 'necessary', 'appointment_with', 'arrival_time', 'departure_time', 'photo']
 
 admin.site.register(Tamu, TamuAdmin)
